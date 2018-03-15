390361abdfd9761205f6f8e85e9799314fb04b06
JarlTuxen <jart@kea.dk>
2018-03-15 14:21:39 +0100

    Demo af arrays

---
+++ Kode/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays{
+
+   public static void main(String[] args){
+      //Bil og Motor - modeller og kod - one-to-one
+      //metodekald med klasser
+      //array af Biler og metode til at udskrive
+      //udskriv Biler i array vha. for loop
+      //refactor med for each
+      
+      //ombytning af to variable temp = x; x = y; y = temp;
+      //metode der tager to Biler og bytter om på deres Motor      
+   }
+}

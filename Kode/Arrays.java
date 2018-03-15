5db5fe19ac5502cc83665bc06345a0abd91f17a0
JarlTuxen <jart@kea.dk>
2018-03-15 14:33:38 +0100

    Demo noter

--- Kode/Arrays.java
+++ Kode/Arrays.java
@@ -7,7 +7,10 @@ public class Arrays{
       //udskriv Biler i array vha. for loop
       //refactor med for each
       
+      //tilføj Dæk til Bil - modeller og kod - one-to-many
+      //array af Dæk i Bil og Bil i Dæk
+      
       //ombytning af to variable temp = x; x = y; y = temp;
-      //metode der tager to Biler og bytter om på deres Motor      
+      //metode der tager to Biler og bytter om på deres Motor
    }
 }

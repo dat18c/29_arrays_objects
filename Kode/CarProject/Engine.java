2f9afec93ce5495531995982bb774cdfcd8a26fe
JarlTuxen <jart@kea.dk>
2018-10-12 13:58:29 +0200

    refactor

--- Kode/CarProject/Engine.java
+++ Kode/CarProject/Engine.java
@@ -4,7 +4,14 @@ public class Engine{
    private int cubik;
    private int hk;
 
-   //constructor
+   //constructors
+   public Engine()
+   {
+      this.brand = "";
+      this.cubik = 0;
+      this.hk = 0;
+   }
+
    public Engine(String brand, int cubik, int hk){
       this.brand = brand;
       this.cubik = cubik;
@@ -12,18 +19,52 @@ public class Engine{
    }
    
    //getters and setters
-   public void setEngine(String brand, int cubik, int hk){
+   public void setBrand(String brand){
       this.brand = brand;
+   }
+
+   public void setCubik(int cubik){
       this.cubik = cubik;
+   }
+
+   public void setHk(int hk){
       this.hk = hk;
    }
    
-   public void print(){
-      System.out.println("Motor");
-      System.out.println("\tMærke:" + brand);
-      System.out.println("\tCubik: " + cubik);
-      System.out.println("\tHk: " + hk);
-      
+   public String getBrand()
+   {
+      return this.brand;
+   }
+   
+   public int getCubik()
+   {
+      return this.cubik;
+   }
+   
+   public int getHk()
+   {
+      return this.hk;
+   }
+   
+   public String toString(){
+      String s = "Motor";
+      s += "\tMærke:" + brand;
+      s += "\tCubik: " + cubik;
+      s += "\tHk: " + hk;
+      return s;
    }
 
+   public boolean equals(Engine eng)
+   {
+      if (this.getBrand().equals(eng.getBrand())
+         && this.getCubik() == eng.getCubik()
+         && this.getHk() == eng.getHk())
+      {
+         return true;
+      }
+      else
+      {
+         return false;
+      }
+   }
 }
--- Kode/CarProject/Tire.java
+++ Kode/CarProject/Tire.java
@@ -1,16 +1,59 @@
 public class Tire{
    private String brand;
-   private int tommer;
+   private int size;
 
-   //constructor
-   public Tire(String brand, int tommer){
-      this.brand = brand;
-      this.tommer = tommer;
+   //constructors
+   public Tire()
+   {
+      this.brand = "";
+      this.size = 0;
    }
    
-   public void print(){
-      System.out.println("Dæk:");
-      System.out.println("\tDæknavn: " + brand);
-      System.out.println("\tStørrelse: " + tommer + "\"");
+   public Tire(String brand, int size)
+   {
+      this.brand = brand;
+      this.size = size;
+   }
+   
+   //getters & setters
+   public void setBrand(String brand)
+   {
+      this.brand = brand;
+   }
+   
+   public void setSize(int size)
+   {
+      this.size = size;
+   }
+   
+   public String getBrand()
+   {
+      return this.brand;
+   }
+   
+   public int getSize()
+   {
+      return this.size;
+   }
+   
+   public String toString()
+   {
+      String s="Dæk:";
+      s+="\tDæknavn: " + getBrand();
+      s+="\tStørrelse: " + getSize() + "\"";
+      return s;
+   }
+   
+   public boolean equals(Tire t)
+   {
+      if (this.getBrand().equals(t.getBrand())
+         && this.getSize() == t.getSize())
+      {
+         return true;
+      }
+      else
+      {
+         return false;
+      }
    }
 }

e885039ac3fc2757120c6951e36e2c811170d8aa
JarlTuxen <jart@kea.dk>
2018-10-15 09:02:32 +0200

    ReverseArray

--- Kode/ReverseArray.java
+++ Kode/ReverseArray.java
@@ -4,13 +4,27 @@ public class ReverseArray
 {
    public static void main(String[] args)
    {
-      int[] tal = { 0, 1, 2, 3};
+      int[] tal = { 0, 1, 2, 3, 4, 5 };
+      System.out.println(Arrays.toString(tal));
       
-      for (int i=0; i<tal.length; i++)
+      for (int i=0; i<tal.length/2; i++)
       {
          //i er det ene element, der skal byttes
          //det andet element regnes ud vha. længde - i
+         System.out.println(i + ", " + (tal.length - 1 - i));
+         // byt de 2 elementer
+         swap(tal, i, tal.length - 1 - i);
       }
       System.out.println(Arrays.toString(tal));
    }
+   
+   public static void swap(int[] tal, int indexa, int indexb)
+   {
+      //lav temp til at gemme den ene variabel midlertidigt
+      int temp;
+      temp = tal[indexa];
+      tal[indexa] = tal[indexb];
+      tal[indexb] = temp;
+   }
+   
 }

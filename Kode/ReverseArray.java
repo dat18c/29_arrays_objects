bc3a678f604e0456b92dc12894e15db735c648d9
JarlTuxen <jart@kea.dk>
2018-10-15 08:31:37 +0200

    ReverseArray

---
+++ Kode/ReverseArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ReverseArray
+{
+   public static void main(String[] args)
+   {
+      int[] tal = { 0, 1, 2, 3};
+      
+      for (int i=0; i<tal.length; i++)
+      {
+         //i er det ene element, der skal byttes
+         //det andet element regnes ud vha. længde - i
+      }
+      System.out.println(Arrays.toString(tal));
+   }
+}
